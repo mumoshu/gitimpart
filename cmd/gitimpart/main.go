feba182b0099f794a9c9bab5b78c2a9e7700be12
Yusuke Kuoka <ykuoka@gmail.com>
2024-07-12 02:48:49 +0000

    Add -var option to the executable for setting jsonnet extVars

--- cmd/gitimpart/main.go
+++ cmd/gitimpart/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/mumoshu/gitimpart"
 )
@@ -19,12 +20,26 @@ func main() {
 func run(args []string) error {
 	flagset := flag.NewFlagSet("gitimpart", flag.ContinueOnError)
 
+	vars := make(map[string]string)
+
 	file := flagset.String("file", "gitimpart.jsonnet", "The configuration file for rendering and pushing files")
 	ghTokenEnv := flagset.String("github-token-env", "GITHUB_TOKEN", "The environment variable name that contains the GitHub token")
 	repo := flagset.String("repo", "", "The repository to push the changes to. It should be in the format of `https://github.com/USER/REPO.git`")
 	branch := flagset.String("branch", "main", "The branch to push the changes to")
 	dryRun := flagset.Bool("dry-run", false, "Print the changes that would be made without actually making them")
 	pullRequest := flagset.Bool("pull-request", false, "Send a pull request to the branch after pushing the changes, instead of pushing directly to the branch")
+
+	flagset.Func("var", "The variables to pass to the jsonnet file. Variables are available via std.extVar(name)", func(v string) error {
+		fields := strings.Split(v, ",")
+		for _, kv := range fields {
+			kv := strings.Split(kv, "=")
+			if len(kv) != 2 {
+				return fmt.Errorf("invalid format for -var: %s", v)
+			}
+			vars[kv[0]] = kv[1]
+		}
+		return nil
+	})
 
 	if err := flagset.Parse(args); err != nil {
 		return fmt.Errorf("failed to parse the flags: %v", err)
@@ -35,7 +50,13 @@ func run(args []string) error {
 		log.Printf("GITHUB_TOKEN is not set. Access to private repositories will be denied unless you configure other means of authentication")
 	}
 
-	r, err := gitimpart.RenderFile(*file)
+	var loadOpts []gitimpart.LoadOption
+
+	if len(vars) > 0 {
+		loadOpts = append(loadOpts, gitimpart.Vars(vars))
+	}
+
+	r, err := gitimpart.RenderFile(*file, loadOpts...)
 	if err != nil {
 		return fmt.Errorf("failed to render file %s: %v", *file, err)
 	}
